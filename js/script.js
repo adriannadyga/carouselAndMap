a71cc0f0ae90da8140523eca2e9f672abb1af0c1
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-07-20 15:42:30 +0200

    Add carousel, reset button and progress bar

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+//flickity
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    hash: true
+});
+
+//button restart
+var restartButton = document.querySelector('.btnRestart');
+
+restartButton.addEventListener( 'click', function() {
+  flkty.select(0);
+});
+
+//progress bar
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+  });
